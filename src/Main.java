d6b494d7985bc0c2b660ae3ab9cf2e17633d374c
0Alina <alina.glodea@gmail.com>
2023-10-05 18:34:47 +0300

    practicat sarcinile de la laborator

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            public static void main(String[] args) {
+                // Declarați variabile
+                int numar = 10;
+                String nume = "John";
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+                // Utilizați o condiție
+                if (numar > 5) {
+                    System.out.println("Numărul este mai mare decât 5.");
+                } else {
+                    System.out.println("Numărul nu este mai mare decât 5.");
+                }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+                // Declarați și utilizați o listă
+                List<String> listaNume = new ArrayList<>();
+                listaNume.add("Alice");
+                listaNume.add("Bob");
+                listaNume.add("Charlie");
+
+                // Utilizați un stream pentru a filtra elementele din listă
+                List<String> rezultatFiltrat = listaNume.stream()
+                        .filter(n -> n.length() > 4)
+                        .collect(Collectors.toList());
+
+                System.out.println("Numele cu lungimea mai mare de 4 caractere: " + rezultatFiltrat);
+
+                try {
+                    // Aruncați o excepție
+                    int rezultat = 5 / 0; // Aruncați o excepție de divizare la zero
+                } catch (ArithmeticException e) {
+                    System.out.println("A apărut o excepție: " + e.getMessage());
+                }
+            }
         }
-    }
-}
